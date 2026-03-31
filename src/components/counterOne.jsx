68c3663904a66bbef8422a97f679ccf6390da836
pavan <rekapallipavan83@gmail.com>
2026-03-31 09:50:43 +0530

    use reducer

---
+++ src/components/counterOne.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useReducer } from "react"
+
+const initialState = 0;
+const reducer = (state, action) => {
+    switch (action) {
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default:
+            return state
+
+    }
+
+}
+export const CounterOne = () => {
+ const [count, dispatch] = useReducer(reducer, initialState)
+    return (
+
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => dispatch('increment')}>Increment </button>
+            <button onClick={() => dispatch('decrement')}>Decrement </button>
+            <button onClick={() => dispatch('reset')}>Reset </button>
+        </div>
+
+    )
+
+}
